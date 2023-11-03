573a48948cf7024242b9e6c6aab445232070adec
rktestNov123 <rkish0501@gmail.com>
2023-11-03 23:56:58 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+  cloud {
+    organization = "test11032023"
+
+    workspaces {
+      name = "Hands-On_With_Terraform_On_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+module "securestorage" {
+  source  = "app.terraform.io/test11032023/securestorage/azurerm"
+  version = "1.0.0"
+  # insert required variables here
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = azurerm_resource_group.rg.name
+  environment          = "Production"
+ 
+  location             = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-41a00029-hands-on-with-terraform-on-azure"
+  location = "southcentralus"
+}
